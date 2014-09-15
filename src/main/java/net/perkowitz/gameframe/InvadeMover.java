7c43fde5b75ce7c30e7b0adbcae1378abf7dfa3a
Mike Perkowitz <mike@perkowitz.net>
2014-09-14 22:20:01 -0700

    Added the flying saucer

--- src/main/java/net/perkowitz/gameframe/InvadeMover.java
+++ src/main/java/net/perkowitz/gameframe/InvadeMover.java
@@ -60,6 +60,9 @@ public class InvadeMover implements MoverInterface {
                 case 2:
                     draw2(frame);
                     return;
+                case 3:
+                    draw3(frame);
+                    return;
             }
         }
     }
@@ -77,6 +80,7 @@ public class InvadeMover implements MoverInterface {
             case 0: return 11;
             case 1: return 8;
             case 2: return 12;
+            case 3: return 16;
             default: return 11;
         }
     }
@@ -256,6 +260,81 @@ public class InvadeMover implements MoverInterface {
         frame.set(currentX+5,currentY-7,color);
         frame.set(currentX+6,currentY-7,color);
         frame.set(currentX+7,currentY-7,color);
+
+    }
+
+    private void draw3(Frame frame) {
+
+        frame.set(currentX+3,currentY,color);
+        frame.set(currentX+12,currentY,color);
+
+        frame.set(currentX+2,currentY-1,color);
+        frame.set(currentX+3,currentY-1,color);
+        frame.set(currentX+4,currentY-1,color);
+        frame.set(currentX+7,currentY-1,color);
+        frame.set(currentX+8,currentY-1,color);
+        frame.set(currentX+11,currentY-1,color);
+        frame.set(currentX+12,currentY-1,color);
+        frame.set(currentX+13,currentY-1,color);
+
+        frame.set(currentX,currentY-2,color);
+        frame.set(currentX+1,currentY-2,color);
+        frame.set(currentX+2,currentY-2,color);
+        frame.set(currentX+3,currentY-2,color);
+        frame.set(currentX+4,currentY-2,color);
+        frame.set(currentX+5,currentY-2,color);
+        frame.set(currentX+6,currentY-2,color);
+        frame.set(currentX+7,currentY-2,color);
+        frame.set(currentX+8,currentY-2,color);
+        frame.set(currentX+9,currentY-2,color);
+        frame.set(currentX+10,currentY-2,color);
+        frame.set(currentX+11,currentY-2,color);
+        frame.set(currentX+12,currentY-2,color);
+        frame.set(currentX+13,currentY-2,color);
+        frame.set(currentX+14,currentY-2,color);
+        frame.set(currentX+15,currentY-2,color);
+
+        frame.set(currentX+1,currentY-3,color);
+        frame.set(currentX+2,currentY-3,color);
+        frame.set(currentX+4,currentY-3,color);
+        frame.set(currentX+5,currentY-3,color);
+        frame.set(currentX+7,currentY-3,color);
+        frame.set(currentX+8,currentY-3,color);
+        frame.set(currentX+10,currentY-3,color);
+        frame.set(currentX+11,currentY-3,color);
+        frame.set(currentX+13,currentY-3,color);
+        frame.set(currentX+14,currentY-3,color);
+
+        frame.set(currentX+2,currentY-4,color);
+        frame.set(currentX+3,currentY-4,color);
+        frame.set(currentX+4,currentY-4,color);
+        frame.set(currentX+5,currentY-4,color);
+        frame.set(currentX+6,currentY-4,color);
+        frame.set(currentX+7,currentY-4,color);
+        frame.set(currentX+8,currentY-4,color);
+        frame.set(currentX+9,currentY-4,color);
+        frame.set(currentX+10,currentY-4,color);
+        frame.set(currentX+11,currentY-4,color);
+        frame.set(currentX+12,currentY-4,color);
+        frame.set(currentX+13,currentY-4,color);
+
+        frame.set(currentX+3,currentY-5,color);
+        frame.set(currentX+4,currentY-5,color);
+        frame.set(currentX+5,currentY-5,color);
+        frame.set(currentX+6,currentY-5,color);
+        frame.set(currentX+7,currentY-5,color);
+        frame.set(currentX+8,currentY-5,color);
+        frame.set(currentX+9,currentY-5,color);
+        frame.set(currentX+10,currentY-5,color);
+        frame.set(currentX+11,currentY-5,color);
+        frame.set(currentX+12,currentY-5,color);
+
+        frame.set(currentX+5,currentY-6,color);
+        frame.set(currentX+6,currentY-6,color);
+        frame.set(currentX+7,currentY-6,color);
+        frame.set(currentX+8,currentY-6,color);
+        frame.set(currentX+9,currentY-6,color);
+        frame.set(currentX+10,currentY-6,color);
 
     }
 
